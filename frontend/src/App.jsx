448eef7f695d13be92dd5631d7d4064505b680e5
shikha <shikhakumari152019@gmail.com>
2025-09-25 00:02:39 +0530

    feat: Add Hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Body from './component/Body'
+import HeroSection from './component/HeroSection'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Body/>}>
-        
+          <Route path="/" element={<HeroSection/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
